cd6b5b76d9b356d4a37937eb60db601d2e8c440e
malinhodev <malinhodev@gmail.com>
2023-12-10 22:17:53 -0300

    hello word

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const mongoose = require('mongoose');
+ 
+mongoose.set("strictQuery", true);
+ 
+mongoose.connect('mongodb://127.0.0.1:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+ 
+const db = mongoose.connection;
+ 
+db.on('error', console.error.bind(console, 'connection error'));
+ 
+db.once('open', function(){
+    console.log("Estamos conectados ao MongoDb!");
+});
